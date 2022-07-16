05e617d9fe20992cf23cf74c94486fc91b339f25
Olnipa <107411395+Olnipa@users.noreply.github.com>
2022-07-16 16:57:06 +0700

    Добавьте файлы проекта.

---
+++ Readint/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Readint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadNumber();
+        }
+        static void ReadNumber()
+        {
+            bool isNumber = true;
+            
+            while (isNumber)
+            {
+                int number;
+                Console.Write("Введите число: ");
+
+                if (int.TryParse(Console.ReadLine(), out number) == true)
+                {
+                    Console.WriteLine(number);
+                    isNumber = false;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели что-то не то. Нужно вести число.");
+                }
+            }
+        }
+    }
+}
